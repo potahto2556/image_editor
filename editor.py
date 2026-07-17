53fd32d60880797fb0675ef59c86b08576677b4e
janhavi <janhavi2556@gmail.com>
2026-07-17 12:37:07 +0530

    Refactor image processing paths to use script directory

--- editor.py
+++ editor.py
@@ -1,13 +1,14 @@
 from PIL import Image, ImageEnhance, ImageFilter
 import os
 
-path = './img'
-path_out = '/final_img'
-# Create the output folder if it doesn't exist yet
-os.makedirs(f".{path_out}", exist_ok=True)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(script_dir, 'img')
+path_out = os.path.join(script_dir, 'final_img')
+
+os.makedirs(path_out, exist_ok=True)
 
 for filename in os.listdir(path):
-    img = Image.open(f"{path}/{filename}")
+    img = Image.open(os.path.join(path, filename))
     edit = img.filter(ImageFilter.SHARPEN).convert('L')
     clean_name = os.path.splitext(filename)[0]
-    edit.save(f".{path_out}/{clean_name}_edited.jpg")
+    edit.save(os.path.join(path_out, f"{clean_name}_edited.jpg"))
